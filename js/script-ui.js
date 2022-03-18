90cbe674cadc2b98ec88ac9416b0506fc25bf603
vanta blanta <vantablanta@gmail.com>
2022-03-18 14:27:54 +0300

    hover effect for nav items, bug: append error, adds css based on window size

--- js/script-ui.js
+++ js/script-ui.js
@@ -9,23 +9,31 @@ $(document).ready(function(){
 
     $(".fa-bars").click(function(){
         $(".bg-img").addClass("fade-bg")
+        $(".hero-section").addClass("hero-size")
         $(this).hide()
         $(".times").show()
-        $(".nav-menu").addClass("nav-menu-show")
+        $(".nav-menu").show()
         $(".logo").hide()
         $(".socials").hide()
         $(".bg-img").animate({marginLeft: "50%"})
+        $("ul").prepend("<p class='nav-text'> Welcome to Moz Pizza Place </p>").stop()
 
     })
     $(".times").click(function(){
         $(".times").hide()
         $(".bg-img").removeClass("fade-bg")
         $(".fa-bars").show()
-        $(".nav-menu").removeClass("nav-menu-show")
-        $(".logo").show()
-        $(".socials").show()
+        $(".nav-menu").hide()
         $(".bg-img").animate({marginLeft: "0"})
+        $(".logo").show()
+        if($(window).width() <= 772){
+            $(".socials").hide() 
+        } else{
+            $(".socials").show()
+        }
+        
     })
+    
 
 
 
