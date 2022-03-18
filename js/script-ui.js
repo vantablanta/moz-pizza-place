49acb2b9b1cc6431e4d848c087c9bb07a9a76970
vanta blanta <vantablanta@gmail.com>
2022-03-18 13:24:20 +0300

    adds a left nav scroll effect, adds a cancel effect, chnages bg style

--- js/script-ui.js
+++ js/script-ui.js
@@ -1,28 +1,31 @@
 $(document).ready(function(){
     function preLoader (){
+        $(".preload-container").show()
         $(".hero-img").hide()
-        $(".preload-container").fadeOut(100) //set to 6000
+        $(".preload-container").fadeOut(100) 
         $(".hero-img").fadeIn(100)
     }
     preLoader()
+
     $(".fa-bars").click(function(){
-
-
-        $(".nav-menu").toggle()
-        $(".nav-menu").animate({left: '0',
-                                right: '50px',
-                            
-                            
-                            }, 2000)
+        $(".bg-img").addClass("fade-bg")
+        $(this).hide()
+        $(".times").show()
+        $(".nav-menu").addClass("nav-menu-show")
+        $(".logo").hide()
+        $(".socials").hide()
+        $(".bg-img").animate({marginLeft: "50%"})
 
     })
-    
-
-    
-    
-    
-
-
+    $(".times").click(function(){
+        $(".times").hide()
+        $(".bg-img").removeClass("fade-bg")
+        $(".fa-bars").show()
+        $(".nav-menu").removeClass("nav-menu-show")
+        $(".logo").show()
+        $(".socials").show()
+        $(".bg-img").animate({marginLeft: "0"})
+    })
 
 
 
