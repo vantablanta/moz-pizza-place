e53658ff5ca918107347cf0b1c5b70fa6e0be21f
vanta blanta <vantablanta@gmail.com>
2022-03-20 21:39:26 +0300

    clears form inpts and resets modal, verifies for empty inputs

--- js/script-ui.js
+++ js/script-ui.js
@@ -1,166 +1,208 @@
-$(document).ready(function(){
-    function preLoader (){
-        $(".preload-container").show()
-        $(".hero-img").hide()
-        $(".preload-container").fadeOut(00) 
-        $(".hero-img").fadeIn(00)
-    }
-    preLoader()
-
-
-
-
-    $("#meat").on('click', function(){
-       // $(".modal").show()
-    })
-
-    $("#meat").mouseover(function(){
-        $(this).addClass("menu-img")
-
-        
-    }) 
-    $("#meat").mouseout(function(){
-        $(this).removeClass("menu-img")
-        
-    }) 
-    $("#hawaian").mouseover(function(){
-        $(this).addClass("menu-img")
-        
-    })
-    $("#hawaian").mouseout(function(){
-        $(this).removeClass("menu-img")
-        
-    })
-    $("#cheese").mouseover(function(){
-        $(this).addClass("menu-img")
-        
-    }) 
-    $("#cheese").mouseout(function(){
-        $(this).removeClass("menu-img")
-        
-    }) 
-    $("#pepperoni").mouseover(function(){
-        $(".menu-text").show()
-        $(this).addClass("menu-img")
-        
-    })
-    $("#pepperoni").mouseout(function(){
-        $(".menu-text").hide()
-        $(this).removeClass("menu-img")
-        
-    }) 
-    $("#bbqChicken").mouseover(function(){
-        $(".menu-text").show()
-        $(this).addClass("menu-img")
-        
-    })
-    $("#bbqChicken").mouseout(function(){
-        $(".menu-text").hide()
-        $(this).removeClass("menu-img")
-        
-    })
-    $("#veggie").mouseover(function(){
-        $(".menu-text").show()
-        $(this).addClass("menu-img")
-        
-    })
-    $("#veggie").mouseout(function(){
-        $(".menu-text").hide()
-        $(this).removeClass("menu-img")
-        
-    }) 
+/*preLoader()
+function preLoader (){
+    $("body").css("overflow", "hidden")
+    $(".preload-container").fadeOut(4000) 
+    $(".hero-container").fadeIn(6000)
+    $(".menu").show()
+    $(".reviews").show()
+    $(".footer").show()
+    $("body").css("overflow-y", "scroll")
     
+}*/
+$(document).ready(function () {
+    $("#meat").on('click', function () {
+        // $(".modal").show()
+    })
 
-    $(".orderBtn1").on('click', function(){
+    $("#meat").mouseover(function () {
+        $(this).addClass("menu-img")
+
+
+    })
+    $("#meat").mouseout(function () {
+        $(this).removeClass("menu-img")
+
+    })
+    $("#hawaian").mouseover(function () {
+        $(this).addClass("menu-img")
+
+    })
+    $("#hawaian").mouseout(function () {
+        $(this).removeClass("menu-img")
+
+    })
+    $("#cheese").mouseover(function () {
+        $(this).addClass("menu-img")
+
+    })
+    $("#cheese").mouseout(function () {
+        $(this).removeClass("menu-img")
+
+    })
+    $("#pepperoni").mouseover(function () {
+        $(".menu-text").show()
+        $(this).addClass("menu-img")
+
+    })
+    $("#pepperoni").mouseout(function () {
+        $(".menu-text").hide()
+        $(this).removeClass("menu-img")
+
+    })
+    $("#bbqChicken").mouseover(function () {
+        $(".menu-text").show()
+        $(this).addClass("menu-img")
+
+    })
+    $("#bbqChicken").mouseout(function () {
+        $(".menu-text").hide()
+        $(this).removeClass("menu-img")
+
+    })
+    $("#veggie").mouseover(function () {
+        $(".menu-text").show()
+        $(this).addClass("menu-img")
+
+    })
+    $("#veggie").mouseout(function () {
+        $(".menu-text").hide()
+        $(this).removeClass("menu-img")
+
+    })
+
+
+    $(".orderBtn1").on('click', function () {
         $(".modal-title").text('Meat Pizza')
     })
-    $(".orderBtn2").on('click', function(){
+    $(".orderBtn2").on('click', function () {
         $(".modal-title").text('Hawaian Pizza')
     })
-    $(".orderBtn3").on('click', function(){
+    $(".orderBtn3").on('click', function () {
         $(".modal-title").text('Cheese Pizza')
     })
-    $(".orderBtn4").on('click', function(){
+    $(".orderBtn4").on('click', function () {
         $(".modal-title").text('Veggie Pizza')
     })
-    $(".orderBtn5").on('click', function(){
+    $(".orderBtn5").on('click', function () {
         $(".modal-title").text('BBQ Chicken Pizza')
     })
-    $(".orderBtn6").on('click', function(){
+    $(".orderBtn6").on('click', function () {
         $(".modal-title").text('Pepperoni Pizza')
     })
 
-    $("#modalSubmit").click( function(){
+    $("#modalSubmit").click(function () {
         formSubmit(e => e.preventDefault())
 
     })
+    $('#formCancelBtn').on('click', function () {
+        $('form')[0].reset()
+    })
+    $("#deliveryCanceled").on('click', function () {
 
+    })
 
-
-    function formSubmit(){
+    function formSubmit() {
         let crust = $('input[name=crust]:checked', 'form').val()
         let toppings = new Array
-        $('input[name=toppings]:checked', 'form').map(function(){
-            toppings.push($(this).val() )
+        $('input[name=toppings]:checked', 'form').map(function () {
+            toppings.push($(this).val())
         })
         let size = $('input[name=size]:checked', 'form').val()
         let delivery = $('input[name=delivery]:checked', 'form').val()
-        let quantity = $('#pizzaQuantity' , 'form').val() 
+        let quantity = $('#pizzaQuantity', 'form').val()
         let pizzaPicked = new Pizza(crust, toppings, size, delivery, quantity)
-
-        let sizeName
-       
-       
-        
 
         let cost = pizzaPicked.calculatePrice()
         let costBd = cost - 200
 
-        if (pizzaPicked.delivery == "true") {
-            $("#deliveryBtn").click() //ask to proceed with delivery or not
-            $("#deliveryProceed").on('click', function(){
-                $("#pizzaDeliveryTotalBtn").click()
-                $("#pizzaDeliveryTotal .modal-body").html(
-                    `
-                    <ul>
-                        <li>Size: </li>
-                        <li>Crust: ${crustName}</li>
-                        <li>Topping: </li>
-                    </ul>
+        let crustName
+        let sizeName
+        let toppingsNames = new Array
+
+        switch (crust) {
+            case "300":
+                crustName = "Crispy Crust";
+                break;
+            case "400":
+                crustName = "Stuffed Crust";
+                break;
+            case "500":
+                crustName = "Gluten Free Crust";
+                break;
+
+        }
+        switch (size) {
+            case "300":
+                sizeName = "Small";
+                break;
+            case "600":
+                sizeName = "Medium";
+                break;
+            case "900":
+                sizeName = "Large";
+                break;
+        }
+        toppings.map(function (i) {
+            if (i == $("#extraCheese").val()) {
+                toppingsNames.push("Extra Cheese")
+            }
+            else if (i == $("#pepperoni").val()) {
+                toppingsNames.push("Pepperoni")
+            }
+            else if (i == $("#bacon").val()) {
+                toppingsNames.push("Bacon")
+            } else if (i == $("#mushroom").val()) {
+                toppingsNames.push("Mushroom")
+            }
+        })
+        if (crustName == undefined || sizeName == undefined || toppingsNames == []) {
+            console.log("No inputs")
+        } else {
+           if (pizzaPicked.delivery == "true") {
+                $("#deliveryBtn").click() //ask to proceed with delivery or not
+                $("#deliveryProceed").on('click', function () {
+                    $("#pizzaDeliveryTotalBtn").click()
+                    $(".content").html(
+                        `
+                    <p>Size: ${sizeName}</p>
+                    <p>Crust: ${crustName}</p>
+                    <p>Toppings: ${toppingsNames} </p>
                     <p>Quantity of Pizza: ${quantity} </p>
                     <p> Total Order Payable: ${costBd}</p>
                     <p>Delivery: Ksh 200 </p>
-                    <p>Total: ${cost}</p>
-                    </p>`
-                )
-            }) 
-        }else{
-            $(".modal-body").html(
-                `
-                <ul>
-                    <li>Size: </li>
-                    <li>Crust: </li>
-                    <li>Topping: </li>
-                </ul>
+                    <p>Total: ${cost}</p>`
+                    )
+                    $("#closeOrderComplete").on('click', function () {
+                        $('form')[0].reset()
+                    })
+
+
+                })
+            } else {
+                $("#pizzaDeliveryTotalBtn").click()
+                $(".content").html(
+                    `
+                <p>Size: ${sizeName} </p>
+                <p>Crust: ${crustName}</p>
+                <p>Toppings: ${toppingsNames} </p>
                 <p>Quantity of Pizza: ${quantity} </p>
-                <p> Total Order Payable: ${costBd}</p>
-                </p>`
-            )
-            $(".btn-close").on('click', function(){
-                $('.modal-body'). 
-                $('#pizzaDeliveryTotal').modal('hide')     
-            })
+                <p> Total Amount: ${costBd}</p>`
+                )
+                $("#closeOrderComplete").on('click', function () {
+                    $('form')[0].reset()
+                })
+
+            }
+
         }
-        $("#thisForm").reset();
-        $('#orderModal').modal('hide') 
+
 
     }
-        
 
 
 })
 
-   
+
 
 
 
